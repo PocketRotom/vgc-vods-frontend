a01ddbeba64381b2ab3904a4b298096035996854
João Costa <joao.costa@proside.pt>
2024-11-18 23:22:12 +0000

    fix: navbar link

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -18,8 +18,13 @@ const NavBar: React.FC = function NavBar() {
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
-          <img src="/logo512.png" alt="App Logo" className="h-8 w-8 mr-2" />
-          <span className="text-white text-lg font-semibold">VGC Vods</span>
+          <Link
+            to="/"
+            className="text-white text-lg font-semibold flex items-center"
+          >
+            <img src="/logo512.png" alt="App Logo" className="h-8 w-8 mr-2" />
+            <span>VGC Vods</span>
+          </Link>
         </div>
         <div className="flex space-x-4">
           <Link to="/" className="text-gray-300 hover:text-white">
